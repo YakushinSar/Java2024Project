62136bdfcf07781b0a388c271c4559e606a36bfa
YakushinSar <saratov482043162@gmail.com>
2024-09-01 22:11:40 +0400

    [+] lesson1

---
+++ src/lesson1/Test.java
@@ -0,0 +1,25 @@
+package lesson1;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Andrey!");
+        System.out.println("");
+
+        // \n переносит строку,
+        // \" добавляет ковычки
+        System.out.println("Andrey! \nAndrey!");
+        System.out.println("Andrey! \n\"Andrey!\"");
+
+        System.out.println("");
+
+        //print без окончения ln не переносит строку
+        System.out.print("Hello ");
+        System.out.print("Andrey!");
+
+        //конкатенация-склеивание
+        System.out.println("");
+        System.out.println("Hello "+"Andrey"+" !!!!");
+            }
+}
+
