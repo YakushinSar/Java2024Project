b2b94459085179e3ae4843a9799f41d58fdda399
YakushinSar <saratov482043162@gmail.com>
2024-09-07 17:42:29 +0400

    [+] Test3

---
+++ src/lesson3/Test3.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        if (a < 100){
+            System.out.println("a меньше 10");
+        }
+    }
+}
