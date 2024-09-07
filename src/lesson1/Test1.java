713230d479727fbb584aa5883e5c948efcaa3875
YakushinSar <saratov482043162@gmail.com>
2024-09-07 10:36:25 +0400

    [+] rename Test1, Test2

--- src/lesson1/Test1.java
+++ src/lesson1/Test1.java
@@ -1,6 +1,6 @@
 package lesson1;
 
-public class Test {
+public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Andrey!");
--- src/lesson2/Test2.java
+++ src/lesson2/Test2.java
@@ -1,6 +1,6 @@
 package lesson2;
 
-public class Test {
+public class Test2 {
     public static void main(String[] args) {
 
 //объявляем переменные
