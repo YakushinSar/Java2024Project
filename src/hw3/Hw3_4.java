a1a02faa53731fe42a1e53405cc1b961eb397a3f
YakushinSar <saratov482043162@gmail.com>
2024-09-08 10:56:51 +0400

    [+] hw 3 4,5,6

---
+++ src/hw3/Hw3_4.java
@@ -0,0 +1,11 @@
+package hw3;
+//Необходимо вывести числа от 0 до 15.
+
+public class Hw3_4 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 16 ; i = i + 1) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/hw3/Hw3_5.java
@@ -0,0 +1,13 @@
+package hw3;
+/*Необходимо вывести все положительные степени числа 5
+которые меньше 10000, вывести результат возведения в степень.
+ */
+
+public class Hw3_5 {
+    public static void main(String[] args) {
+        for (int i = 5; i < 10000; i = i * 5) {
+            System.out.println(i);
+
+        }
+    }
+}
