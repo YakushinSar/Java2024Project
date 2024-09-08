189393df0adb9e1bb0ccfd4ea39c770bf6be2db1
YakushinSar <saratov482043162@gmail.com>
2024-09-08 11:23:07 +0400

    [+] refactor hw3

--- src/hw3/Hw3_1.java
+++ src/hw3/Hw3_1.java
@@ -11,12 +11,10 @@ public class Hw3_1 {
 
         if (a == b) {
             System.out.println("a == b");
+        } else if (a < b) {
+            System.out.println("a < b");
         } else {
-            if (a < b) {
-                System.out.println("a < b");
-            } else {
-                System.out.println("a > b");
-            }
+            System.out.println("a > b");
         }
     }
 }
